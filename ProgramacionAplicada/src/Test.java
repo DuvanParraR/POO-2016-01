8f809521d287df976fc2fef13c5e857f4547f5e8
FAMILIA <FAMILIA@FAMILIA-PC>
2016-02-13 12:47:40 -0500

    Cambios en la clase principal

--- ProgramacionAplicada/src/Test.java
+++ ProgramacionAplicada/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		System.out.println("Prueba con Control de Versiones");
+		System.out.println("Realizando un cambio en el codigo fuente");
 	}
 
 }
